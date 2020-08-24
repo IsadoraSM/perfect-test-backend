db03490f5d7b5eef70ee43375deccf1811d94a89
ialisadora <isadorasoaresmotta@gmail.com>
2020-08-24 20:52:36 -0300

    cria relacionamento entre Product e Sale

--- app/Models/Sale.php
+++ app/Models/Sale.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Sale extends Model
 {
     protected $fillable = ['name', 'uuid', 'description', 'price', 'local_image'];
+
+    public function product(){
+        return $this->belongsTo('App\Models\Product');
+    }
 }
